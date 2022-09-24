acfab2bf307703752f759b8c8027742aaa337dd3
Omar Mahat <omar.mahat@student.moringaschool.com>
2022-09-24 14:58:48 +0300

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -12,6 +12,27 @@ class BirdsController < ApplicationController
     render json: bird, status: :created
   end
 
+  # UPDATE /birds/:id
+  def update
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
@@ -25,7 +46,7 @@ class BirdsController < ApplicationController
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
